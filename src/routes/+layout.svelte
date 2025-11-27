be5329dcb782b374b772316d4c359b400a6f3311
aarsh21 <aarshpersonal@proton.me>
2025-11-27 18:29:58 +0530

    refactor: simplify layout by removing unused Toaster component
    Remove svelte-sonner Toaster since no toast notifications are used
    in the dictation UI after removing SettingsPage.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,9 @@
 <script lang="ts">
 	import './layout.css';
-	import { Toaster } from "$lib/components/ui/sonner";
 	import { ModeWatcher } from "mode-watcher";
 	
 	let { children } = $props();
 </script>
 
 <ModeWatcher defaultMode="dark" />
-<Toaster richColors position="bottom-center" />
 {@render children()}
